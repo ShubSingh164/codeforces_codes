c46c1eb0b8b18092f23ba53406cde40a522367e3
Shubham <125245720+ShubSingh164@users.noreply.github.com>
2024-07-31 16:06:28 +0530

    Create 1979A-Guess_the_maximum.cpp

---
+++ 1979A-Guess_the_maximum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int>a(n);
+	    for (int i=0; i<n; i++){
+	        cin>>a[i];
+	    }
+	    vector<int> b;
+	    for (int i=0 ; i<n-1; i++){
+	        b.push_back(max(a[i],a[i+1]));
+	    }
+	    sort (b.begin(),b.end());
+	    cout<<b[0]-1<<endl;
+	}
+	return 0;
+
+}
