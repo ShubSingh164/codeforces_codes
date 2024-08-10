771a3552bce361bc6cc1d57a524d1f77097492d5
Shubham <125245720+ShubSingh164@users.noreply.github.com>
2024-08-11 00:26:16 +0530

    Create 1154A-Restoring_three_number.cpp

---
+++ 1154A-Restoring_three_number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 4;
+    vector<int> arr(n);  
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+    int a = arr[3] - arr[2];
+     int b = abs(a - arr[0]);
+     int c = abs(b - arr[2]);
+     cout<< a<<" "<<b<<" " <<c<< endl; 
+ 
+    
+    return 0;
+}
