4a3fbda92e537b5c95d09c061978a11d1c87d68a
Shubham <125245720+ShubSingh164@users.noreply.github.com>
2024-08-11 16:29:29 +0530

    Create 1283A-Minute_Before_New_Year.cpp

---
+++ 1283A-Minute_Before_New_Year.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int h, m;
+        cin>>h>>m;
+        int ans = 0;
+        if (h == 23){
+          ans = 60 - m;
+        }else{
+            ans = (23 - h)*60 + (60-m);
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
