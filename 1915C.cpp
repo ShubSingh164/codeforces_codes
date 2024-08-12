8126fe1ec77072171c8c621a55e77a79bf4759d5
Shubham <125245720+ShubSingh164@users.noreply.github.com>
2024-08-13 01:37:08 +0530

    Update 1915C.cpp

--- 1915C.cpp
+++ 1915C.cpp
@@ -1,5 +1,36 @@
 // 1915C Can I Square?
 
+
+
+correct accepeted code:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+   
+        long long sum = accumulate(arr.begin(), arr.end(), 0LL);
+
+        long long sqrtSum = (long long)sqrt(sum);
+    
+        if (sqrtSum * sqrtSum == sum) {
+            cout << "YES\n";  
+        } else {
+            cout << "NO\n";   
+        }
+    }
+    return 0;
+}
+
+code which failed on 3rd testcase.
 #include <bits/stdc++.h>
 using namespace std;
 
