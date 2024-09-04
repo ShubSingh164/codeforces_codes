2515864eb41e71527691a8fabe0932fd3378c546
Shubham <125245720+ShubSingh164@users.noreply.github.com>
2024-09-04 22:51:48 +0530

    Create 1987A-upload_more_RAM(4-sep).cpp

---
+++ 1987A-upload_more_RAM(4-sep).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n,k;
+    cin>>n>>k;
+    
+    int a = n * k;
+    int b = k - 1;
+    
+    cout<<a - b<< endl;
+}
+
+}
